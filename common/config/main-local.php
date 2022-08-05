d934453a16ae14c83b736e771891e1331b3f3ece
jaywebdevpoc <webdevelopmentpoc@gmail.com>
2022-08-05 16:37:36 +0530

    config files

---
+++ common/config/main-local.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Lombardia Informatica S.p.A.
+ * OPEN 2.0
+ *
+ *
+ * @package    lispa\amos\basic\template
+ * @category   CategoryName
+ */
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=dfp',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 88000,
+            'schemaCache' => 'schemaCache',
+            'attributes'=>[
+                PDO::ATTR_PERSISTENT => true
+            ]
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'support@gmail.com',
+                'password' => '',
+                // 'port' => '587',//587//465
+                //'encryption' => 'tls',
+                'port' => '587',//587//
+                'encryption' => 'tls',                
+            ]
+        ],
+    ],
+];
---
+++ common/config/params-local.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Lombardia Informatica S.p.A.
+ * OPEN 2.0
+ *
+ *
+ * @package    lispa\amos\basic\template
+ * @category   CategoryName
+ */
+
+return [
+    'adminEmail' => 'support@gmail.com',
+    'supportEmail' => 'support@gmail.com',
+    'email-assistenza' => 'support@gmail.com',
+    'user.passwordResetTokenExpire' => 86400,
+    'google_places_api_key' => '',
+    'google_recaptcha_site_key' => '',
+    'platform' => [
+        'backendUrl' => 'http://localhost',         //TODO REPLACE YOUR URL
+        'frontendUrl' => 'http://localhost',       //TODO REPLACE YOUR URL
+    ],
+];
